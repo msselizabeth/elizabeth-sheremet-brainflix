2c94c780b9666b0ae82d40aab249a63fcc2a980d
Elizabeth <lizagotovitryby@gmail.com>
2024-11-09 23:15:53 -0500

    fix path to video

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,18 @@
 import "./Hero.scss"
-
+import { baseUrl } from "../../App";
+const apiKey = "f62229a7-4276-4ce1-a592-6df6e8407873";
 
 const Hero = ({video: {image, video}}) => {
   return (
     <div className="video__container">
       <video
-        controls
+       controls
         width="100%"
         height="auto"
-        poster={image}
+        poster={`${baseUrl}/${image}`}
         className="video"
       >
-        <source src={video} type="video/mp4" />
+        <source src={`${video}?api_key=${apiKey}`} type="video/mp4" />
       </video>
     </div>
   );
